8942cae9f82322f7b83d4de6244e1890436040d4
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-02-02 15:40:05 -0300

    funcion para realizar el tiro

---
+++ src/main/java/physic/MainPhysic.java
@@ -0,0 +1,28 @@
+package physic;
+
+public class MainPhysic {
+
+    public static void main(String[] args) {
+
+        double speed = 80.0;
+        double deltaTime = 1.0;
+        double x = 0.0;
+        double y = 0.0;
+        double ay = -9.8;
+        double ax = 0;
+        double angle = 45;
+        double vx = speed * Math.cos(angle * (Math.PI / 180.0));
+        double vy = speed * Math.sin(angle * (Math.PI / 180.0));
+
+        double time = 0.0;
+        while (y >= 0) {
+            time += deltaTime;
+            x += vx * deltaTime;
+            y += vy * deltaTime;
+
+            vx += ax * deltaTime;
+            vy += ay * deltaTime;
+            System.out.println("x: " + x + ", y: " + y);
+        }
+    }
+}
